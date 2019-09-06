e9791509628b4db9056f90498881b1a03913a488
Dmitri Stasenko <StaDimVyach@mail.ru>
2019-09-06 22:16:25 +0300

    Add Authentication in project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,28 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req) {
-    return req.user;
-  }
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
